440009a72703e9c4eb011af4ab63dfb754387970
MS0073 <mudassir7472@gmail.com>
2026-05-22 17:13:54 +0500

    Create functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,19 @@
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+string passwordGenerator(int length){
+
+    srand(time(0));
+
+    string password;
+    string data = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(){}[]:<>/?";
+
+    for (int i=1; i <= 16; i++){
+        password = password + data[rand() % data.length()];
+    }
+
+    return password;
+
+}
